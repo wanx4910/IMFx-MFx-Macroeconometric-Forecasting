fe9226f10ee1e2b3d8e9960514c5b10484b66e14
wanx4910 <52807760+wanx4910@users.noreply.github.com>
2019-09-05 16:26:01 +0800

    Update Module_2_Fiji_Demonstration.R

--- Module_2_Fiji_Demonstration.R
+++ Module_2_Fiji_Demonstration.R
@@ -1,10 +1,8 @@
 setwd("Desktop/IMFx Macroeconometric Forecasting")
 FIJI <- read.csv("Module 2/Fiji Dataset/Module_2_Fiji_fiji.csv")
-x <- lm(log(RGDP_FIJI) ~ log(RGDP_AUS), data = FIJI, na.action = na.exclude
+x <- lm(log(RGDP_FIJI) ~ log(RGDP_AUS), data = FIJI)
 summary(x) ## to see the results
 
 x_resid <- resid(x)
 
-library(ggplot2)
-
-plot(FIJI$Date[1:44],x_resid, type = "l")
+plot(FIJI$Date[1:44], x_resid, type = "l")
